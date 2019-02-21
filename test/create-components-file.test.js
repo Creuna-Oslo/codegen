7456eaccc8ca4110e31e720088a005e62460757c
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-02-21 13:26:41 +0100

    Refactors tests using test macro

--- test/create-components-file.test.js
+++ test/create-components-file.test.js
@@ -1,32 +1,14 @@
-const fs = require('fs');
-const fsExtra = require('fs-extra');
 const path = require('path');
-const tempy = require('tempy');
 const test = require('ava');
 
-const { createComponentsFile } = require('../index');
+const testWithFixture = require('./helpers/test-with-fixture');
 
-const fixturePath = path.join(__dirname, 'fixtures', 'components');
-
-test('Creates components file', t => {
-  const outputPath = tempy.directory();
-  fsExtra.copySync(fixturePath, outputPath);
-
-  createComponentsFile({
-    fileHeader: '//header',
-    fileName: 'components.js',
-    outputPath,
-    searchPath: outputPath
-  });
-
-  const expected = `//header
-
-import A from "./a";
+test('Creates components file', testWithFixture, {
+  fixturePath: path.join(__dirname, 'fixtures', 'components'),
+  testSubject: require('../index').createComponentsFile,
+  expected: `import A from "./a";
 import B from "./b";
 
 export { A, B };
-`;
-
-  const fileContent = fs.readFileSync(path.join(outputPath, 'components.js'), 'utf8');
-  t.is(expected, fileContent);
+`
 });
--- test/create-pages-file.test.js
+++ test/create-pages-file.test.js
@@ -1,27 +1,12 @@
-const fs = require('fs');
-const fsExtra = require('fs-extra');
 const path = require('path');
-const tempy = require('tempy');
 const test = require('ava');
 
-const { createPagesFile } = require('../index');
+const testWithFixture = require('./helpers/test-with-fixture');
 
-const fixturePath = path.join(__dirname, 'fixtures', 'pages');
-
-test('Creates components file', t => {
-  const outputPath = tempy.directory();
-  fsExtra.copySync(fixturePath, outputPath);
-
-  createPagesFile({
-    fileHeader: '//header',
-    fileName: 'pages.js',
-    outputPath,
-    searchPath: outputPath
-  });
-
-  const expected = `//header
-
-import A from "./a";
+test('Creates pages file', testWithFixture, {
+  fixturePath: path.join(__dirname, 'fixtures', 'pages'),
+  testSubject: require('../index').createPagesFile,
+  expected: `import A from "./a";
 import B from "./b";
 
 export default [
@@ -38,8 +23,5 @@ export default [
     path: "/b"
   }
 ];
-`;
-
-  const fileContent = fs.readFileSync(path.join(outputPath, 'pages.js'), 'utf8');
-  t.is(expected, fileContent);
+`
 });
